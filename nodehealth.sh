3c60948786d1036fa22d22f25600c93369dbc117
valipasha <valipashashaik26@gmail.com>
2022-12-06 12:06:19 +0530

    node health

---
+++ nodehealth.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#####################################
+#
+#author : Sk Valipasha
+#date : 06/12/2022
+#
+#this script outputs the node health
+#
+#version: 1.0
+#####################################
+
+df -h
+free -g
+nproc
