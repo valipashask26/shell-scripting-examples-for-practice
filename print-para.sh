790b5288689cc973b9d8cd667805ddc2e2a690e2
valipasha sk <70925407+valipashask26@users.noreply.github.com>
2023-12-08 15:51:38 +0530

    Update print-para.sh

--- print-para.sh
+++ print-para.sh
@@ -2,17 +2,4 @@
 
 # Get the directory of the current script
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-
-_namespace() {
-    if [[ -z "$1" ]]; then
-        ns="aqua"
-    else
-        ns=$1
-    fi
-}
-
-_namespace "$@"
-
-# Now you can use the 'ns' and 'script_dir' variables wherever needed in your script
-echo "Selected namespace: $ns"
 echo "Script directory: $script_dir"
